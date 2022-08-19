e109c5564057bdd71eb17ad2856173b3b628d1ea
nick <nickdeveloper@protonmail.com>
2022-08-19 15:15:36 +0300

    fix

--- init.lua
+++ init.lua
@@ -1,37 +1,9 @@
 
 
 local function disable_formatting(client)
-	client.resolved_capabilities.document_formatting = false
-	client.resolved_capabilities.document_range_formatting = false
-end
-
-local dap = require('dap')
-local jester = require('jester')
-dap.adapters.node2 = {
-  type = 'executable',
-  command = 'node',
-  args = {os.getenv('HOME') .. '/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js'},
-}
-dap.configurations.javascript = {
-  {
-    name = 'Launch',
-    type = 'node2',
-    request = 'launch',
-    program = '${file}',
-    cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    console = 'integratedTerminal',
-  },
-  {
-    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-    name = 'Attach to process',
-    type = 'node2',
-    request = 'attach',
-    processId = require'dap.utils'.pick_process,
-  },
-}
-
+		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
+	end
 local config = {
   -- Configure AstroNvim updates
   updater = {
@@ -78,16 +50,6 @@ local config = {
   plugins = {
     -- Add plugins, the packer syntax without the "use"
     init = {
-    	{'mfussenegger/nvim-dap'},
-    	{ 
-    		'David-Kunz/jester',
-				config = function()
-          jester.setup {
-						path_to_jest_run = './node_modules/.bin/jest',
-						path_to_jest_debug = './node_modules/.bin/jest'
-          }
-        end
-    	},
     	{'neomake/neomake'},
     	{
 				'kevinhwang91/nvim-bqf', 
@@ -110,213 +72,200 @@ local config = {
         'nvim-treesitter/nvim-treesitter-context',
       },
       -- {
-      	--   'beauwilliams/focus.nvim',
-      	--   config = function() require("focus").setup({number = false, treewidth = 40}) end
-      	-- },
-      	{
-        	's1n7ax/nvim-window-picker',
-        	tag = 'v1.*',
-        	config = function()
-          	require'window-picker'.setup()
-        	end,
-      	},
-      	{
-        	'ggandor/lightspeed.nvim'
-      	},
-      	{
-       		"navarasu/onedark.nvim",
-        	as = "onedark",
-        	config = function()
-          	require('onedark').setup {
-            	style = 'dark'
-          	}
-          	require('onedark').load()
-        	end,
-      	},
-      	["declancm/cinnamon.nvim"] = { disable = true },
-      	-- You can disable default plugins as follows:
-      	-- ["goolord/alpha-nvim"] = { disable = true },
+      --   'beauwilliams/focus.nvim',
+      --   config = function() require("focus").setup({number = false, treewidth = 40}) end
+      -- },
+      {
+        's1n7ax/nvim-window-picker',
+        tag = 'v1.*',
+        config = function()
+          require'window-picker'.setup()
+        end,
+      },
+      {
+        'ggandor/lightspeed.nvim'
+      },
+      {
+       "navarasu/onedark.nvim",
+        as = "onedark",
+        config = function()
+          require('onedark').setup {
+            style = 'dark'
+          }
+          require('onedark').load()
+        end,
+      },
+      ["declancm/cinnamon.nvim"] = { disable = true },
+      -- You can disable default plugins as follows:
+      -- ["goolord/alpha-nvim"] = { disable = true },
 
-    	},
-    	["neo-tree"] = function(config)
-    		config.close_if_last_window = false
-      	return config
-    	end,
-    	["telescope"] = function(config)
-				local telescope_actions = require "telescope.actions"
-      	config.defaults.mappings.n['<C-q>'] = telescope_actions.close
-      	config.defaults.mappings.i['<C-q>'] = telescope_actions.close
-      	return config
-    	end,
-    	["null-ls"] = function(config)
-      	local null_ls = require "null-ls"
-      	config.sources = {
-        	null_ls.builtins.formatting.eslint_d,
-        	null_ls.builtins.diagnostics.eslint_d,
-        	null_ls.builtins.code_actions.eslint_d,
-      	}
-      	config.on_attach = function(client)
-        	-- NOTE: You can remove this on attach function to disable format on save
-        	if client.resolved_capabilities.document_formatting then
-          	vim.api.nvim_create_autocmd("BufWritePre", {
-            	desc = "Auto format before save",
-            	pattern = "<buffer>",
-            	callback = function () vim.lsp.buf.formatting_sync(nil, 3000) end,
-          	})
-        	end
-      	end
-      	return config
-    	end,
-    	treesitter = {
-      	ensure_installed = { "lua" },
-    	},
-    	["nvim-lsp-installer"] = {
-      	ensure_installed = { "sumneko_lua" },
-    	},
-    	packer = {
-      	compile_path = vim.fn.stdpath "data" .. "/packer_compiled.lua",
-    	},
+    },
+    ["neo-tree"] = function(config)
+    	config.close_if_last_window = false
+      return config
+    end,
+    ["telescope"] = function(config)
+			local telescope_actions = require "telescope.actions"
+      config.defaults.mappings.n['<C-q>'] = telescope_actions.close
+      config.defaults.mappings.i['<C-q>'] = telescope_actions.close
+      return config
+    end,
+    ["null-ls"] = function(config)
+      local null_ls = require "null-ls"
+      config.sources = {
+        null_ls.builtins.formatting.eslint_d,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.code_actions.eslint_d,
+      }
+      config.on_attach = function(client)
+        -- NOTE: You can remove this on attach function to disable format on save
+        if client.resolved_capabilities.document_formatting then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            desc = "Auto format before save",
+            pattern = "<buffer>",
+            callback = function () vim.lsp.buf.formatting_sync(nil, 3000) end,
+          })
+        end
+      end
+      return config
+    end,
+    treesitter = {
+      ensure_installed = { "lua" },
+    },
+    ["nvim-lsp-installer"] = {
+      ensure_installed = { "sumneko_lua" },
+    },
+    packer = {
+      compile_path = vim.fn.stdpath "data" .. "/packer_compiled.lua",
+    },
 
-  	},
+  },
 
-  	-- LuaSnip Options
-  	luasnip = {
-    	-- Add paths for including more VS Code style snippets in luasnip
-    	vscode_snippet_paths = {},
-    	-- Extend filetypes
-    	filetype_extend = {
-      	javascript = { "javascriptreact" },
-    	},
-  	},
+  -- LuaSnip Options
+  luasnip = {
+    -- Add paths for including more VS Code style snippets in luasnip
+    vscode_snippet_paths = {},
+    -- Extend filetypes
+    filetype_extend = {
+      javascript = { "javascriptreact" },
+    },
+  },
 
-  	-- Modify which-key registration
-  	["which-key"] = {
-    	-- Add bindings
-    	register_mappings = {
-      	-- first key is the mode, n == normal mode
-      	n = {
-        	-- second key is the prefix, <leader> prefixes
-        	["<leader>"] = {
-          	-- which-key registration table for normal mode, leader prefix
-          	-- ["N"] = { "<cmd>tabnew<cr>", "New Buffer" },
-        	},
-      	},
-    	},
-  	},
+  -- Modify which-key registration
+  ["which-key"] = {
+    -- Add bindings
+    register_mappings = {
+      -- first key is the mode, n == normal mode
+      n = {
+        -- second key is the prefix, <leader> prefixes
+        ["<leader>"] = {
+          -- which-key registration table for normal mode, leader prefix
+          -- ["N"] = { "<cmd>tabnew<cr>", "New Buffer" },
+        },
+      },
+    },
+  },
 
-  	-- CMP Source Priorities
-  	-- modify here the priorities of default cmp sources
-  	-- higher value == higher priority
-  	-- The value can also be set to a boolean for disabling default sources:
-  	-- false == disabled
-  	-- true == 1000
-  	cmp = {
-    	source_priority = {
-      	nvim_lsp = 1000,
-      	luasnip = 750,
-      	buffer = 500,
-      	path = 250,
-    	},
-  	},
+  -- CMP Source Priorities
+  -- modify here the priorities of default cmp sources
+  -- higher value == higher priority
+  -- The value can also be set to a boolean for disabling default sources:
+  -- false == disabled
+  -- true == 1000
+  cmp = {
+    source_priority = {
+      nvim_lsp = 1000,
+      luasnip = 750,
+      buffer = 500,
+      path = 250,
+    },
+  },
 
-  	-- Extend LSP configuration
-  	lsp = {
-    	-- enable servers that you already have installed without lsp-installer
-    	servers = {
-      	-- "pyright"
-    	},
-    	-- easily add or disable built in mappings added during LSP attaching
-    	mappings = {
-      	n = {
-        	-- ["<leader>lf"] = false -- disable formatting keymap
-      	},
-    	},
-    	-- add to the server on_attach function
-    	-- on_attach = function(client, bufnr)
-    		-- end,
+  -- Extend LSP configuration
+  lsp = {
+    -- enable servers that you already have installed without lsp-installer
+    servers = {
+      -- "pyright"
+    },
+    -- easily add or disable built in mappings added during LSP attaching
+    mappings = {
+      n = {
+        -- ["<leader>lf"] = false -- disable formatting keymap
+      },
+    },
+    -- add to the server on_attach function
+    -- on_attach = function(client, bufnr)
+    -- end,
 
-    		-- override the lsp installer server-registration function
-    		-- server_registration = function(server, opts)
-    			--   require("lspconfig")[server].setup(opts)
-    			-- end,
+    -- override the lsp installer server-registration function
+    -- server_registration = function(server, opts)
+    --   require("lspconfig")[server].setup(opts)
+    -- end,
 
-    			-- Add overrides for LSP server settings, the keys are the name of the server
-    			["server-settings"] = {
-      			tsserver = {
-        			root_dir = require('lspconfig.util').find_git_ancestor,
-      			},
-      			eslint = {
-        			on_attach = disable_formatting,
-        			settings = {
-          			workingDirectories = {
-            			{ mode = 'location' }
-          			}
-        			},
-        			root_dir = require('lspconfig.util').find_git_ancestor,
-      			},
-    			},
-  			},
+    -- Add overrides for LSP server settings, the keys are the name of the server
+    ["server-settings"] = {
+      tsserver = {
+        root_dir = require('lspconfig.util').find_git_ancestor,
+      },
+      eslint = {
+        on_attach = disable_formatting,
+        settings = {
+          workingDirectories = {
+            { mode = 'location' }
+          }
+        },
+        root_dir = require('lspconfig.util').find_git_ancestor,
+      },
+    },
+  },
 
-  			-- Diagnostics configuration (for vim.diagnostics.config({}))
-  			diagnostics = {
-    			virtual_text = true,
-    			underline = true,
-  			},
+  -- Diagnostics configuration (for vim.diagnostics.config({}))
+  diagnostics = {
+    virtual_text = true,
+    underline = true,
+  },
 
-  			mappings = {
-    			-- first key is the mode
-    			n = {
+  mappings = {
+    -- first key is the mode
+    n = {
+    	["<leader>lT"] = { ":Trouble workspace_diagnostics<cr>", desc="Open Trouble Workspace Diagnostics"},
+    	["<leader>lt"] = { ":Trouble document_diagnostics<cr>", desc="Open Trouble Document"},
+      ["tn"]= { ":tabnew<cr>", desc="New Tab"},
+      -- second key is the lefthand side of the map
+      ["<C-s>"] = { ":w!<cr>", desc = "Save File" },
+    },
+    t = {
+      -- setting a mapping to false will disable it
+      -- ["<esc>"] = false,
+    },
+  },
 
-    				["<leader>jr"] = { function() jester.run_file(
-    					{
-								path_to_jest_run = './node_modules/.bin/jest',
-								path_to_jest_debug = './node_modules/.bin/jest'
-    					}
-    					) end, desc="Debug Jest Test"},
-    				["<leader>jd"] = { function() jester.debug_file(
-    					{
-								path_to_jest_run = './node_modules/.bin/jest',
-								path_to_jest_debug = './node_modules/.bin/jest'
-    					}
-    					) end, desc="Debug Jest Test"},
-    				["<leader>lT"] = { ":Trouble workspace_diagnostics<cr>", desc="Open Trouble Workspace Diagnostics"},
-    				["<leader>lt"] = { ":Trouble document_diagnostics<cr>", desc="Open Trouble Document"},
-      			["tn"]= { ":tabnew<cr>", desc="New Tab"},
-      			-- second key is the lefthand side of the map
-      			["<C-s>"] = { ":w!<cr>", desc = "Save File" },
-    			},
-    			t = {
-      			-- setting a mapping to false will disable it
-      			-- ["<esc>"] = false,
-    			},
-  			},
+  -- This function is run last
+  -- good place to configuring augroups/autocommands and custom filetypes
+  polish = function()
+    -- Set key binding
+    -- Set autocommands
+    vim.api.nvim_create_augroup("packer_conf", { clear = true })
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      desc = "Sync packer after modifying plugins.lua",
+      group = "packer_conf",
+      pattern = "plugins.lua",
+      command = "source <afile> | PackerSync",
+    })
 
-  			-- This function is run last
-  			-- good place to configuring augroups/autocommands and custom filetypes
-  			polish = function()
-    			-- Set key binding
-    			-- Set autocommands
-    			vim.api.nvim_create_augroup("packer_conf", { clear = true })
-    			vim.api.nvim_create_autocmd("BufWritePost", {
-      			desc = "Sync packer after modifying plugins.lua",
-      			group = "packer_conf",
-      			pattern = "plugins.lua",
-      			command = "source <afile> | PackerSync",
-    			})
+    -- Set up custom filetypes
+    -- vim.filetype.add {
+    --   extension = {
+    --     foo = "fooscript",
+    --   },
+    --   filename = {
+    --     ["Foofile"] = "fooscript",
+    --   },
+    --   pattern = {
+    --     ["~/%.config/foo/.*"] = "fooscript",
+    --   },
+    -- }
+  end,
+}
 
-    			-- Set up custom filetypes
-    			-- vim.filetype.add {
-    				--   extension = {
-    					--     foo = "fooscript",
-    					--   },
-    					--   filename = {
-    						--     ["Foofile"] = "fooscript",
-    						--   },
-    						--   pattern = {
-    							--     ["~/%.config/foo/.*"] = "fooscript",
-    							--   },
-    							-- }
-  							end,
-							}
-
-							return config
+return config
