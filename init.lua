3b348ba79b0d11bff4dba1b2bc94d21bfb25eec0
nick <nickdeveloper@protonmail.com>
2022-10-06 17:56:35 +0300

    filter damn react.d.ts from lsp when running definition on JSX

--- init.lua
+++ init.lua
@@ -3,6 +3,19 @@ local function disable_formatting(client)
   client.resolved_capabilities.document_range_formatting = false
 end
 
+local function filter(arr, fn)
+  if type(arr) ~= "table" then return arr end
+
+  local filtered = {}
+  for k, v in pairs(arr) do
+    if fn(v, k, arr) then table.insert(filtered, v) end
+  end
+
+  return filtered
+end
+
+local function filterReactDTS(value) return string.match(value.targetUri, "react/index.d.ts") == nil end
+
 local config = {
   updater = {
     channel = "nightly",
@@ -48,6 +61,16 @@ local config = {
     ["server-settings"] = {
       tsserver = {
         root_dir = require("lspconfig.util").find_git_ancestor,
+        handlers = {
+          ["textDocument/definition"] = function(err, result, method, ...)
+            if vim.tbl_islist(result) and #result > 1 then
+              local filtered_result = filter(result, filterReactDTS)
+              return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
+            end
+
+            vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
+          end,
+        },
       },
       eslint = {
         on_attach = disable_formatting,
