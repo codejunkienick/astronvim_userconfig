f7b46f98dcba5ef0d80df55b1e14117023d981ae
nick <nickdeveloper@protonmail.com>
2022-10-19 14:06:38 +0300

    close buffers in telescope

--- init.lua
+++ init.lua
@@ -141,6 +141,7 @@ local config = {
     telescope = function(config)
       local telescope_actions = require "telescope.actions"
       config.defaults.mappings.n["<C-q>"] = telescope_actions.close
+      config.defaults.mappings.n["d"] = telescope_actions.delete_buffer
       config.defaults.mappings.i["<C-q>"] = telescope_actions.close
       return config
     end,
